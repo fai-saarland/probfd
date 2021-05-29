628797473bb260b7fe2134169e3230800e085d22
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2021-05-30 00:55:01 +0200

    return infinity for missing states in expcost projection

--- src/search/mdps/heuristics/pdbs/expected_cost/expcost_projection.cc
+++ src/search/mdps/heuristics/pdbs/expected_cost/expcost_projection.cc
@@ -32,7 +32,7 @@ value_type::value_t
 ExpCostProjection::get_value(const GlobalState &state) const {
     auto abstract_state = state_mapper_->operator()(state);
     std::optional v = value_table.get_optional(abstract_state);
-    return v ? *v : std::numeric_limits<value_type::value_t>::max();
+    return v ? *v : std::numeric_limits<value_type::value_t>::infinity();
 }
 
 unsigned int ExpCostProjection::num_reachable_states() const {
