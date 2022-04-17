9a4c24027f24851fcfc6a8149948088996fd0446
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2022-04-17 16:48:51 +0200

    polish idual

--- src/search/mdps/engines/idual.h
+++ src/search/mdps/engines/idual.h
@@ -33,21 +33,11 @@ struct Statistics {
         out << "  LP Variables: " << lp_variables << std::endl;
         out << "  LP Constraints: " << lp_constraints << std::endl;
     }
-
-    void register_prints(ProgressReport* report) const
-    {
-        report->register_print(
-            [this](std::ostream& out) { out << "iteration=" << iterations; });
-    }
 };
 
 struct PerStateInfo {
-    static const uint8_t NEW = 0;
-    static const uint8_t OPEN = 1;
-    static const uint8_t CLOSED = 2;
-    static const uint8_t TERMINAL = 3;
-
-    unsigned idx = (unsigned)-1;
+    enum : uint8_t { NEW, OPEN, CLOSED, TERMINAL };
+    unsigned idx = std::numeric_limits<unsigned>::max();
     uint8_t status = NEW;
 };
 
@@ -57,44 +47,40 @@ struct FrontierStateInfo {
 
 class ValueGroup {
 public:
-    explicit ValueGroup()
-        : values()
-        , indices(Comparator(&values))
-    {
-    }
-
-    const value_type::value_t& operator[](const unsigned& i) const
+    const value_type::value_t& operator[](unsigned i) const
     {
         return values[i];
     }
 
-    unsigned get_id(const value_type::value_t& val)
+    unsigned get_id(value_type::value_t val)
     {
         values.push_back(val);
         auto it = indices.insert(values.size() - 1);
         if (!it.second) {
             values.pop_back();
         }
+
         return *it.first;
     }
 
 private:
     struct Comparator {
-        explicit Comparator(const std::vector<value_type::value_t>* values)
+        explicit Comparator(const std::vector<value_type::value_t>& values)
             : values(values)
         {
         }
 
-        bool operator()(const unsigned& x, const unsigned& y) const
+        bool operator()(unsigned x, unsigned y) const
         {
-            return values->at(x) < values->at(y);
+            return values[x] < values[y];
         }
 
-        const std::vector<value_type::value_t>* values;
+        const std::vector<value_type::value_t>& values;
     };
 
     std::vector<value_type::value_t> values;
-    std::set<unsigned, Comparator> indices;
+    std::set<unsigned, Comparator> indices =
+        std::set<unsigned, Comparator>(Comparator(values));
 };
 
 /**
@@ -129,9 +115,6 @@ public:
         , value_initializer_(value_initializer)
         , dead_end_value_(this->get_minimal_reward())
         , lp_solver_(solver_type)
-        , state_infos_()
-        , terminals_()
-        , statistics_()
     {
     }
 
@@ -153,6 +136,7 @@ public:
                 objective_ = value_type::value_t(eval);
                 return;
             }
+
             lp::LPVariable var((value_type::value_t)eval, inf, 1.0);
             lp_solver_.load_problem(
                 lp::LPObjectiveSense::MINIMIZE,
@@ -172,110 +156,113 @@ public:
         std::vector<double> lp_sol;
 
         report_->register_value("v", [this] { return objective_; });
-        statistics_.register_prints(report_);
+        report_->register_print([&](std::ostream& out) {
+            out << "iteration=" << statistics_.iterations;
+        });
 
         do {
             ++statistics_.iterations;
             statistics_.expansions += frontier.size();
 
-            for (int i = frontier.size() - 1; i >= 0; --i) {
-                const StateID state_id = frontier[i];
+            for (const StateID state_id : frontier) {
                 const State state = this->lookup_state(state_id);
                 auto rew = this->get_state_reward(state);
                 const unsigned var_id = state_infos_[state_id].idx;
                 assert(state_infos_[state_id].status == PerStateInfo::CLOSED);
+
                 if (rew) {
-                    lp_solver_.set_variable_lower_bound(
-                        var_id,
-                        (value_type::value_t)rew);
-                } else {
-                    lp_solver_.set_variable_lower_bound(
-                        var_id,
-                        dead_end_value_);
-                    this->generate_all_successors(state_id, aops, transitions);
-                    for (unsigned j = 0; j < transitions.size(); ++j) {
-                        Distribution<StateID>& transition = transitions[j];
-                        transition.make_unique();
-                        if (transition.size() == 1 &&
-                            transition.begin()->first == state_id) {
+                    const auto value = static_cast<value_type::value_t>(rew);
+                    lp_solver_.set_variable_lower_bound(var_id, value);
+                    continue;
+                }
+
+                lp_solver_.set_variable_lower_bound(var_id, dead_end_value_);
+                this->generate_all_successors(state_id, aops, transitions);
+
+                for (unsigned j = 0; j < transitions.size(); ++j) {
+                    Distribution<StateID>& transition = transitions[j];
+                    transition.make_unique();
+
+                    if (transition.size() == 1 &&
+                        transition.begin()->first == state_id) {
+                        continue;
+                    }
+
+                    lp::LPConstraint c(-inf, inf);
+
+                    double base_val =
+                        (value_type::value_t)rew +
+                        this->get_action_reward(state_id, aops[j]);
+                    StateID next_prev_state = prev_state;
+                    double w = 1.0;
+
+                    for (const auto [succ_id, prob] : transition) {
+                        if (succ_id == state_id) {
+                            w -= prob;
                             continue;
                         }
 
-                        lp::LPConstraint c(-inf, inf);
+                        PerStateInfo& succ_info = state_infos_[succ_id];
 
-                        double base_val =
-                            (value_type::value_t)rew +
-                            this->get_action_reward(state_id, aops[j]);
-                        StateID next_prev_state = prev_state;
-                        double w = 1.0;
+                        if (succ_id > prev_state) {
+                            assert(state_infos_[succ_id].idx == (unsigned)-1);
 
-                        for (auto it = transition.begin();
-                             it != transition.end();
-                             ++it) {
-                            const StateID succ_id = it->first;
-                            PerStateInfo& succ_info = state_infos_[succ_id];
-                            if (succ_id == state_id) {
-                                w -= it->second;
-                            } else if (succ_id > prev_state) {
-                                assert(
-                                    state_infos_[succ_id].idx == (unsigned)-1);
-                                State succ_state = this->lookup_state(succ_id);
-                                auto eval =
-                                    value_initializer_->operator()(succ_state);
-                                if (eval) {
-                                    succ_info.status = PerStateInfo::TERMINAL;
-                                    succ_info.idx = terminals_.get_id(
-                                        (value_type::value_t)eval);
-                                    base_val += it->second *
-                                                ((value_type::value_t)eval);
-                                } else {
-                                    lp_solver_.add_variable(
-                                        lp::LPVariable(
-                                            (value_type::value_t)eval,
-                                            inf,
-                                            0.0),
-                                        std::vector<int>(),
-                                        std::vector<double>());
-                                    succ_info.idx = next_var_id;
-                                    succ_info.status = PerStateInfo::OPEN;
-                                    c.insert(next_var_id, -it->second);
-                                    open_states[succ_id].incoming.push_back(
-                                        next_constraint_id);
-                                    ++next_var_id;
-                                }
-                                if (succ_id > next_prev_state) {
-                                    next_prev_state = succ_id;
-                                }
+                            State succ_state = this->lookup_state(succ_id);
+                            auto eval =
+                                value_initializer_->operator()(succ_state);
+                            const auto value = (value_type::value_t)eval;
+
+                            if (eval) {
+                                succ_info.status = PerStateInfo::TERMINAL;
+                                succ_info.idx = terminals_.get_id(value);
+                                base_val += prob * value;
                             } else {
-                                assert(succ_info.status != PerStateInfo::NEW);
-                                switch (succ_info.status) {
-                                case (PerStateInfo::OPEN):
-                                    open_states[succ_id].incoming.push_back(
-                                        next_constraint_id);
-                                    [[fallthrough]];
-                                case (PerStateInfo::CLOSED):
-                                    c.insert(succ_info.idx, -it->second);
-                                    break;
-                                default:
-                                    base_val +=
-                                        it->second * terminals_[succ_info.idx];
-                                    break;
-                                }
+                                lp_solver_.add_variable(
+                                    lp::LPVariable(value, inf, 0.0),
+                                    std::vector<int>(),
+                                    std::vector<double>());
+                                succ_info.idx = next_var_id;
+                                succ_info.status = PerStateInfo::OPEN;
+                                c.insert(next_var_id, -prob);
+                                open_states[succ_id].incoming.push_back(
+                                    next_constraint_id);
+                                ++next_var_id;
+                            }
+
+                            if (succ_id > next_prev_state) {
+                                next_prev_state = succ_id;
+                            }
+                        } else {
+                            assert(succ_info.status != PerStateInfo::NEW);
+
+                            switch (succ_info.status) {
+                            case (PerStateInfo::OPEN):
+                                open_states[succ_id].incoming.push_back(
+                                    next_constraint_id);
+                                [[fallthrough]];
+                            case (PerStateInfo::CLOSED):
+                                c.insert(succ_info.idx, -prob);
+                                break;
+                            default:
+                                base_val += prob * terminals_[succ_info.idx];
+                                break;
                             }
                         }
-
-                        prev_state = next_prev_state;
-
-                        assert(w > 0);
-                        c.insert(var_id, w);
-                        c.set_lower_bound(base_val);
-                        lp_solver_.add_constraint(c);
-                        ++next_constraint_id;
                     }
-                    aops.clear();
-                    transitions.clear();
+
+                    prev_state = next_prev_state;
+
+                    assert(w > value_type::zero);
+                    c.insert(var_id, w);
+                    c.set_lower_bound(base_val);
+                    lp_solver_.add_constraint(c);
+                    ++next_constraint_id;
                 }
+
+                aops.clear();
+                transitions.clear();
             }
+
             frontier.clear();
 
             lp_solver_.solve();
@@ -286,10 +273,11 @@ public:
             auto it = open_states.begin();
             while (it != open_states.end()) {
                 double sum = 0;
-                const auto& refs = it->second.incoming;
-                for (unsigned i = 0; i < refs.size(); ++i) {
-                    sum += lp_sol[refs[i]];
+
+                for (auto& r : it->second.incoming) {
+                    sum += lp_sol[r];
                 }
+
                 if (sum > eps) {
                     StateID id(it->first);
                     state_infos_[id].status = PerStateInfo::CLOSED;
@@ -302,6 +290,7 @@ public:
 
             report_->operator()();
         } while (!frontier.empty());
+
         assert(lp_sol.size() > 0);
 
         statistics_.lp_variables = next_var_id;
