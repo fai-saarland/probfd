f78b00832d99010d564f106b0bd7fb49f84f84b4
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-01-23 19:29:08 +0100

    remove obsolete forward declaration

--- include/search/probfd/heuristics/pdbs/engine_interfaces.h
+++ include/search/probfd/heuristics/pdbs/engine_interfaces.h
@@ -20,13 +20,6 @@
 #include <unordered_set>
 #include <vector>
 
-namespace legacy {
-namespace successor_generator {
-template <typename T>
-class SuccessorGenerator;
-}
-} // namespace legacy
-
 namespace pdbs {
 class PatternDatabase;
 }
