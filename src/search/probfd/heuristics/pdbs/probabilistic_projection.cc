fai-saarland/probfd

commit ffc7cad04320cbec4c135006c4fb9c506d41c6ef

make use of c++ 20 ranges

src/search/probfd/heuristics/pdbs/probabilistic_projection.cc
@@ -3,6 +3,7 @@
 #include "probfd/task_utils/task_properties.h"
 
 #include <algorithm>
+#include <compare>
 #include <deque>
 #include <numeric>
 #include <set>
@@ -23,27 +24,20 @@ struct ProgressionOperatorFootprint {
         : precondition_hash(precondition_hash)
         , successors(op.outcomes.begin(), op.outcomes.end())
     {
-        std::sort(successors.begin(), successors.end());
+        std::ranges::sort(successors);
     }
 
-    friend bool operator<(
+    friend auto operator<=>(
         const ProgressionOperatorFootprint& a,
-        const ProgressionOperatorFootprint& b)
-    {
-        return std::tie(a.precondition_hash, a.successors) <
-               std::tie(b.precondition_hash, b.successors);
-    }
+        const ProgressionOperatorFootprint& b) = default;
 };
 
 struct OutcomeInfo {
     StateRank base_effect = StateRank(0);
     std::vector<int> missing_pres;
 
-    friend bool operator==(const OutcomeInfo& a, const OutcomeInfo& b)
-    {
-        return std::tie(a.base_effect, a.missing_pres) ==
-               std::tie(b.base_effect, b.missing_pres);
-    }
+    friend auto
+    operator<=>(const OutcomeInfo& a, const OutcomeInfo& b) = default;
 };
 
 } // namespace
@@ -107,13 +101,11 @@ ProbabilisticProjection::StateRankSpace::StateRankSpace(
             }
 
             for (const auto& [var, val] : local_effect) {
-                auto beg = utils::make_transform_iterator(
-                    local_precondition.begin(),
+                auto pre_it = std::ranges::lower_bound(
+                    local_precondition,
+                    var,
+                    std::ranges::less(),
                     &FactPair::var);
-                auto end = utils::make_transform_iterator(
-                    local_precondition.end(),
-                    &FactPair::var);
-                auto pre_it = utils::find_sorted(beg, end, var);
 
                 int val_change = val;
 
@@ -121,7 +113,7 @@ ProbabilisticProjection::StateRankSpace::StateRankSpace(
                     vars_eff_not_pre.emplace_back(var, 0);
                     info.missing_pres.push_back(var);
                 } else {
-                    val_change -= pre_it.base->value;
+                    val_change -= pre_it->value;
                 }
 
                 info.base_effect += mapper.from_fact(var, val_change);
@@ -156,11 +148,9 @@ ProbabilisticProjection::StateRankSpace::StateRankSpace(
             std::vector<FactPair> precondition;
             precondition.reserve(local_precondition.size() + values.size());
 
-            std::merge(
-                local_precondition.begin(),
-                local_precondition.end(),
-                values.begin(),
-                values.end(),
+            std::ranges::merge(
+                local_precondition,
+                values,
                 std::back_inserter(precondition));
 
             if (operator_pruning) {

include/search/abstract_task.h
@@ -23,17 +23,7 @@ struct FactPair {
         : var(var), value(value) {
     }
 
-    bool operator<(const FactPair &other) const {
-        return var < other.var || (var == other.var && value < other.value);
-    }
-
-    bool operator==(const FactPair &other) const {
-        return var == other.var && value == other.value;
-    }
-
-    bool operator!=(const FactPair &other) const {
-        return var != other.var || value != other.value;
-    }
+    friend auto operator<=>(const FactPair&, const FactPair&) = default;
 
     /*
       This special object represents "no such fact". E.g., functions

include/search/probfd/distribution.h
@@ -4,11 +4,12 @@
 #include "probfd/value_type.h"
 
 #include "utils/iterators.h"
-#include "utils/range_proxy.h"
 #include "utils/rng.h"
 
 #include <algorithm>
 #include <cassert>
+#include <compare>
+#include <ranges>
 #include <utility>
 #include <vector>
 
@@ -54,39 +55,10 @@ public:
     {
     }
 
-    /// Compares two item-probability pairs lexigographically by operator<.
-    friend bool operator<(
+    /// Comparison of two item-probability pairs.
+    friend auto operator<=>(
         const ItemProbabilityPair<T>& left,
-        const ItemProbabilityPair<T>& right)
-    {
-        return std::tie(left.item, left.probability) <
-               std::tie(right.item, right.probability);
-    }
-
-    /// Checks for equality of two item-probability pairs.
-    friend bool operator==(
-        const ItemProbabilityPair<T>& left,
-        const ItemProbabilityPair<T>& right)
-    {
-        return std::tie(left.item, left.probability) ==
-               std::tie(right.item, right.probability);
-    }
-
-    /// Compares the items of two item-probability pairs by operator<.
-    static bool key_less(
-        const ItemProbabilityPair<T>& left,
-        const ItemProbabilityPair<T>& right)
-    {
-        return left.item < right.item;
-    }
-
-    /// Compares the items of two item-probability pairs by operator==.
-    static bool key_equal(
-        const ItemProbabilityPair<T>& left,
-        const ItemProbabilityPair<T>& right)
-    {
-        return left.item == right.item;
-    }
+        const ItemProbabilityPair<T>& right) = default;
 };
 
 /**
@@ -172,12 +144,18 @@ public:
 
     iterator find(const T& t)
     {
-        return std::find(elem_begin(), elem_end(), t).base;
+        return std::ranges::find(
+            distribution_,
+            t,
+            &ItemProbabilityPair<T>::item);
     }
 
     const_iterator find(const T& t) const
     {
-        return std::find(elem_begin(), elem_end(), t).base;
+        return std::ranges::find(
+            distribution_,
+            t,
+            &ItemProbabilityPair<T>::item);
     }
 
     /**
@@ -190,7 +168,7 @@ public:
      */
     bool is_dirac(const T& t) const
     {
-        return size() == 1 && *elem_begin() == t;
+        return size() == 1 && distribution_.front().item == t;
     }
 
     /**
@@ -232,10 +210,10 @@ public:
             return;
         }
 
-        std::sort(
-            distribution_.begin(),
-            distribution_.end(),
-            ItemProbabilityPair<T>::key_less);
+        std::ranges::sort(
+            distribution_,
+            std::ranges::less{},
+            &ItemProbabilityPair<T>::item);
 
         auto last = std::unique(
             distribution_.begin(),
@@ -293,74 +271,32 @@ public:
 
     auto end() const { return distribution_.end(); }
 
-    auto elem_begin()
+    auto elements()
     {
-        return utils::make_transform_iterator(
-            begin(),
+        return std::views::transform(
+            distribution_,
             &ItemProbabilityPair<T>::item);
     }
-
-    auto elem_begin() const
-    {
-        return utils::make_transform_iterator(
-            begin(),
-            &ItemProbabilityPair<T>::item);
-    }
-
-    auto elem_end()
-    {
-        return utils::make_transform_iterator(
-            end(),
-            &ItemProbabilityPair<T>::item);
-    }
-
-    auto elem_end() const
-    {
-        return utils::make_transform_iterator(
-            end(),
-            &ItemProbabilityPair<T>::item);
-    }
-
-    auto elements() { return utils::make_range(elem_begin(), elem_end()); }
 
     auto elements() const
     {
-        return utils::make_range(elem_begin(), elem_end());
+        return std::views::transform(
+            distribution_,
+            &ItemProbabilityPair<T>::item);
     }
 
-    auto prob_begin()
+    auto probabilities()
     {
-        return utils::make_transform_iterator(
-            begin(),
+        return std::ranges::transform(
+            distribution_,
             &ItemProbabilityPair<T>::probability);
     }
-
-    auto prob_begin() const
-    {
-        return utils::make_transform_iterator(
-            begin(),
-            &ItemProbabilityPair<T>::probability);
-    }
-
-    auto prob_end()
-    {
-        return utils::make_transform_iterator(
-            end(),
-            &ItemProbabilityPair<T>::probability);
-    }
-
-    auto prob_end() const
-    {
-        return utils::make_transform_iterator(
-            end(),
-            &ItemProbabilityPair<T>::probability);
-    }
-
-    auto probabilities() { return utils::make_range(prob_begin(), prob_end()); }
 
     auto probabilities() const
     {
-        return utils::make_range(prob_begin(), prob_end());
+        return std::ranges::transform(
+            distribution_,
+            &ItemProbabilityPair<T>::probability);
     }
 
     /**
@@ -373,17 +309,8 @@ public:
      */
     const distribution_t& data() const { return distribution_; }
 
-    friend bool
-    operator<(const Distribution<T>& left, const Distribution<T>& right)
-    {
-        return left.distribution_ < right.distribution_;
-    }
-
-    friend bool
-    operator==(const Distribution<T>& left, const Distribution<T>& right)
-    {
-        return left.distribution_ == right.distribution_;
-    }
+    friend auto
+    operator<=>(const Distribution<T>&, const Distribution<T>&) = default;
 };
 
 } // namespace probfd

include/search/probfd/heuristics/pdbs/engine_interfaces.h
@@ -12,9 +12,9 @@
 #include "probfd/engine_interfaces/transition_generator.h"
 
 #include "utils/collections.h"
-#include "utils/range_proxy.h"
 
 #include <memory>
+#include <ranges>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
@@ -30,7 +30,7 @@ template <>
 class StateIDMap<heuristics::pdbs::StateRank> {
 public:
     using visited_iterator = std::set<int>::const_iterator;
-    using visited_range = utils::RangeProxy<visited_iterator>;
+    using visited_range = std::ranges::subrange<visited_iterator>;
 
     explicit StateIDMap() = default;
 

include/search/probfd/heuristics/pdbs/state_rank.h
@@ -25,10 +25,6 @@ struct StateRank {
     {
     }
 
-    bool operator==(const StateRank& x) const { return id == x.id; }
-    bool operator<(const StateRank& x) const { return id < x.id; }
-    bool operator>(const StateRank& x) const { return id > x.id; }
-
     StateRank operator+(const StateRank& x) const
     {
         return StateRank(x.id + id);
@@ -50,6 +46,8 @@ struct StateRank {
         id -= x.id;
         return *this;
     }
+
+    friend auto operator<=>(StateRank a, StateRank b) = default;
 };
 
 extern std::ostream& operator<<(std::ostream& out, const StateRank& s);

include/search/probfd/heuristics/pdbs/state_ranking_function.h
@@ -7,10 +7,8 @@
 #include "probfd/task_proxy.h"
 #include "probfd/types.h"
 
-#include "utils/iterators.h"
-#include "utils/range_proxy.h"
-
 #include <memory>
+#include <ranges>
 #include <string>
 #include <utility>
 #include <vector>
@@ -44,18 +42,18 @@ class StateRankingFunction {
 
 public:
     class PartialAssignmentIterator {
-        using difference_type = void;
+        std::vector<FactPair> partial_state_;
+        const std::vector<VariableInfo>* var_infos_;
+
+        bool done;
+
+    public:
+        using difference_type = std::ptrdiff_t;
         using value_type = std::vector<FactPair>;
         using pointer = std::vector<FactPair>*;
         using reference = std::vector<FactPair>&;
         using iterator_category = std::forward_iterator_tag;
 
-        std::vector<FactPair> partial_state_;
-        const std::vector<VariableInfo>& var_infos_;
-
-        bool done;
-
-    public:
         PartialAssignmentIterator(
             std::vector<FactPair> partial_state,
             const std::vector<VariableInfo>& var_infos);
@@ -63,25 +61,20 @@ public:
         PartialAssignmentIterator& operator++();
         PartialAssignmentIterator& operator--();
 
+        PartialAssignmentIterator operator++(int);
+        PartialAssignmentIterator operator--(int);
+
         reference operator*();
         pointer operator->();
 
-        friend bool operator==(
-            const PartialAssignmentIterator& a,
-            const utils::default_sentinel_t&);
+        friend bool
+        operator==(const PartialAssignmentIterator& a, std::default_sentinel_t);
 
-        friend bool operator!=(
-            const PartialAssignmentIterator& a,
-            const utils::default_sentinel_t&);
+        friend bool
+        operator!=(const PartialAssignmentIterator& a, std::default_sentinel_t);
     };
 
     class StateRankIterator {
-        using difference_type = void;
-        using value_type = const StateRank;
-        using pointer = const StateRank*;
-        using reference = const StateRank&;
-        using iterator_category = std::forward_iterator_tag;
-
         std::vector<int> values_;
         std::vector<int> domains_;
         std::vector<long long int> multipliers_;
@@ -90,21 +83,28 @@ public:
         bool done;
 
     public:
+        using difference_type = std::ptrdiff_t;
+        using value_type = const StateRank;
+        using pointer = const StateRank*;
+        using reference = const StateRank&;
+        using iterator_category = std::forward_iterator_tag;
+
         StateRankIterator(
             StateRank state,
             const std::vector<int>& indices,
             const std::vector<VariableInfo>& var_infos);
 
         StateRankIterator& operator++();
+        StateRankIterator operator++(int);
 
         reference operator*();
         pointer operator->();
 
         friend bool
-        operator==(const StateRankIterator&, const utils::default_sentinel_t&);
+        operator==(const StateRankIterator&, std::default_sentinel_t);
 
         friend bool
-        operator!=(const StateRankIterator&, const utils::default_sentinel_t&);
+        operator!=(const StateRankIterator&, std::default_sentinel_t);
     };
 
 public:
@@ -170,17 +170,17 @@ public:
     PartialAssignmentIterator
     partial_assignments_begin(std::vector<FactPair> partial_state) const;
 
-    utils::default_sentinel_t partial_assignments_end() const;
+    std::default_sentinel_t partial_assignments_end() const;
 
-    utils::RangeProxy<PartialAssignmentIterator, utils::default_sentinel_t>
+    std::ranges::subrange<PartialAssignmentIterator, std::default_sentinel_t>
     partial_assignments(std::vector<FactPair> partial_state) const;
 
     StateRankIterator
     state_ranks_begin(StateRank offset, std::vector<int> indices) const;
 
-    utils::default_sentinel_t state_ranks_end() const;
+    std::default_sentinel_t state_ranks_end() const;
 
-    utils::RangeProxy<StateRankIterator, utils::default_sentinel_t>
+    std::ranges::subrange<StateRankIterator, std::default_sentinel_t>
     state_ranks(StateRank offset, std::vector<int> indices) const;
 
     long long int get_multiplier(int var) const;

include/search/probfd/preprocessing/end_component_decomposition.h
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <iterator>
 #include <memory>
+#include <ranges>
 #include <type_traits>
 #include <vector>
 
@@ -600,15 +601,15 @@ private:
                     transitions += it->aops.size();
                 }
 
-                auto begin = utils::make_transform_iterator(
-                    scc_begin,
-                    &StackInfo::stateid);
-                auto end = utils::make_transform_iterator(
-                    scc_end,
-                    &StackInfo::stateid);
+                auto sid_range = std::ranges::subrange(scc_begin, scc_end) |
+                                 std::views::transform(&StackInfo::stateid);
                 auto abegin =
                     utils::make_transform_iterator(scc_begin, &StackInfo::aops);
-                sys_->build_quotient(begin, end, scc_repr_id, abegin);
+                sys_->build_quotient(
+                    sid_range.begin(),
+                    sid_range.end(),
+                    scc_repr_id,
+                    abegin);
                 stack_.erase(scc_begin, scc_end);
 
                 // Update stats

include/search/probfd/quotient_system.h
@@ -12,6 +12,7 @@
 #include <algorithm>
 #include <cassert>
 #include <memory>
+#include <ranges>
 #include <set>
 #include <unordered_set>
 #include <vector>
@@ -104,7 +105,7 @@ public:
     const_iterator begin() const;
     const_iterator end() const;
 
-    utils::RangeProxy<QuotientStateIDIterator>
+    std::ranges::subrange<QuotientStateIDIterator, QuotientStateIDIterator>
     quotient_range(const StateID& state_id) const;
 
     StateID translate_state_id(const StateID& sid) const;
@@ -132,7 +133,7 @@ public:
     template <typename Range>
     void build_quotient(Range& range)
     {
-        this->build_quotient(range.begin(), range.end());
+        this->build_quotient(std::begin(range), std::end(range));
     }
 
     template <typename StateIDIterator>

commit 998493a1a43b0b087baf27a885e77fc84d658e0e

fix bug introduced in commit ffc7cad04320cbec4c135006c4fb9c506d41c6ef

src/search/probfd/heuristics/pdbs/probabilistic_projection.cc
@@ -109,7 +109,7 @@ ProbabilisticProjection::StateRankSpace::StateRankSpace(
 
                 int val_change = val;
 
-                if (pre_it == local_precondition.end()) {
+                if (pre_it == local_precondition.end() || pre_it->var != var) {
                     vars_eff_not_pre.emplace_back(var, 0);
                     info.missing_pres.push_back(var);
                 } else {