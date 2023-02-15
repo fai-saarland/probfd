69b46d92b3214ad4e7dfd3d342ed2939ce51e2a9
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-02-15 17:46:57 +0100

    move implementations to correct source file

--- src/search/probfd/value_type.cc
+++ src/search/probfd/value_type.cc
@@ -27,21 +27,6 @@ value_t abs(value_t val)
     return std::abs(val);
 }
 
-bool is_approx_equal(value_t v1, value_t v2, value_t tolerance)
-{
-    return v1 == v2 || std::abs(v1 - v2) <= tolerance;
-}
-
-bool is_approx_less(value_t v1, value_t v2, value_t tolerance)
-{
-    return v1 + tolerance < v2;
-}
-
-bool is_approx_greater(value_t v1, value_t v2, value_t tolerance)
-{
-    return v1 - tolerance > v2;
-}
-
 value_t g_epsilon = 5e-5;
 
 } // namespace probfd
--- src/search/probfd/value_utils.cc
+++ src/search/probfd/value_utils.cc
@@ -5,6 +5,24 @@
 
 namespace probfd {
 
+bool is_approx_equal(value_t v1, value_t v2, value_t tolerance)
+{
+    assert(tolerance >= 0.0_vt);
+    return v1 == v2 || std::abs(v1 - v2) <= tolerance;
+}
+
+bool is_approx_less(value_t v1, value_t v2, value_t tolerance)
+{
+    assert(tolerance >= 0.0_vt);
+    return v1 + tolerance < v2;
+}
+
+bool is_approx_greater(value_t v1, value_t v2, value_t tolerance)
+{
+    assert(tolerance >= 0.0_vt);
+    return v1 - tolerance > v2;
+}
+
 Interval::Interval(value_t val)
     : Interval(val, val)
 {
