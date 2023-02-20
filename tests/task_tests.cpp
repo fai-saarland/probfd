7b9cb81c9b7f1873cc61b8fcccc6d3429225ec4e
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-02-20 16:34:33 +0100

    added task tests

---
+++ tests/task_tests.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include "probfd/tasks/root_task.h"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+TEST(TaskTests, test_read_sas_task)
+{
+    std::filesystem::path file_path = __FILE__;
+    std::fstream file(file_path.remove_filename() / "sas_files/test1.sas");
+    auto task = probfd::tasks::read_sas_task(file);
+
+    ASSERT_EQ(task->get_num_axioms(), 0);
+    ASSERT_EQ(task->get_num_variables(), 11);
+    ASSERT_EQ(task->get_num_operators(), 260);
+    ASSERT_EQ(
+        task->get_initial_state_values(),
+        std::vector({1, 0, 0, 0, 1, 6, 6, 5, 1, 6, 0}));
+    ASSERT_EQ(task->get_num_goals(), 7);
+}
