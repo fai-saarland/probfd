4b6413f8ed151bb7f6cff81768405300cd65c6da
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-02-24 15:25:21 +0100

    add split selector

--- cmake/DownwardFiles.cmake
+++ cmake/DownwardFiles.cmake
@@ -1198,6 +1198,7 @@ fast_downward_plugin(
         probfd/heuristics/cartesian/abstraction
         probfd/heuristics/cartesian/probabilistic_transition
         probfd/heuristics/cartesian/probabilistic_transition_system
+        probfd/heuristics/cartesian/split_selector
         probfd/heuristics/cartesian/subtask_generators
         probfd/heuristics/cartesian/utils
     DEPENDS CEGAR ADDITIVE_HEURISTIC EXTRA_PROBABILISTIC_TASKS
