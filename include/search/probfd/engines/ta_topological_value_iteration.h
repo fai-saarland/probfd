fai-saarland/probfd

commit 01cf1bb5b9cdef7e0dd875b5febb85f56fbc83f8

TVI: simplify self loop normalization

include/search/probfd/engines/ta_topological_value_iteration.h
@@ -145,7 +145,6 @@ class TATopologicalValueIteration : public MDPEngine<State, Action> {
 
     struct QValueInfo {
         // Probability to remain in the same state.
-        // Casted to the self-loop normalization factor after finalize().
         value_t self_loop_prob = 0.0_vt;
 
         // Precomputed part of the Q-value.
@@ -164,17 +163,13 @@ class TATopologicalValueIteration : public MDPEngine<State, Action> {
 
         bool finalize()
         {
-            if (self_loop_prob != 0.0_vt) {
-                // Calculate self-loop normalization factor
-                self_loop_prob = 1.0_vt / (1.0_vt - self_loop_prob);
-
-                if (scc_successors.empty()) {
-                    // Apply self-loop normalization immediately
-                    conv_part *= self_loop_prob;
-                }
+            if (!scc_successors.empty()) {
+                assert(0_vt < self_loop_prob && self_loop_prob < 1_vt);
+                conv_part *= 1_vt / (1_vt - self_loop_prob);
+                return false;
             }
 
-            return scc_successors.empty();
+            return true;
         }
 
         template <typename ValueStore>
@@ -184,10 +179,6 @@ class TATopologicalValueIteration : public MDPEngine<State, Action> {
 
             for (auto& [state_id, prob] : scc_successors) {
                 res += prob * value_store[state_id];
-            }
-
-            if (self_loop_prob != 0.0_vt) {
-                res *= self_loop_prob;
             }
 
             return res;

include/search/probfd/engines/topological_value_iteration.h
@@ -140,7 +140,6 @@ class TopologicalValueIteration : public MDPEngine<State, Action> {
         ActionID action_id;
 
         // Probability to remain in the same state.
-        // Casted to the self-loop normalization factor after finalize().
         value_t self_loop_prob = 0_vt;
 
         // Precomputed part of the Q-value.
@@ -160,17 +159,13 @@ class TopologicalValueIteration : public MDPEngine<State, Action> {
 
         bool finalize()
         {
-            if (self_loop_prob != 0_vt) {
-                // Calculate self-loop normalization factor
-                self_loop_prob = 1_vt / (1_vt - self_loop_prob);
-
-                if (nconv_successors.empty()) {
-                    // Apply self-loop normalization immediately
-                    conv_part *= self_loop_prob;
-                }
+            if (!nconv_successors.empty()) {
+                assert(0_vt < self_loop_prob && self_loop_prob < 1_vt);
+                conv_part *= 1 / (1_vt - self_loop_prob);
+                return false;
             }
 
-            return nconv_successors.empty();
+            return true;
         }
 
         EngineValueType compute_q_value() const
@@ -179,10 +174,6 @@ class TopologicalValueIteration : public MDPEngine<State, Action> {
 
             for (auto& [value, prob] : nconv_successors) {
                 res += prob * (*value);
-            }
-
-            if (self_loop_prob != 0_vt) {
-                res *= self_loop_prob;
             }
 
             return res;

commit 1e93258c429eb0f9ebb39637951a21f3cb499f17

[bugfix] TVI: also normalize distribution Fixes bug introduced in commit 01cf1bb5b9cdef7e0dd875b5febb85f56fbc83f8

include/search/probfd/engines/ta_topological_value_iteration.h
@@ -164,8 +164,18 @@ class TATopologicalValueIteration : public MDPEngine<State, Action> {
         bool finalize()
         {
             if (!scc_successors.empty()) {
-                assert(0_vt < self_loop_prob && self_loop_prob < 1_vt);
-                conv_part *= 1_vt / (1_vt - self_loop_prob);
+                if (self_loop_prob != 0_vt) {
+                    assert(self_loop_prob > 0_vt && self_loop_prob < 1_vt);
+
+                    const auto normalization = 1_vt / (1_vt - self_loop_prob);
+
+                    for (auto& [_, prob] : scc_successors) {
+                        prob *= normalization;
+                    }
+
+                    conv_part *= normalization;
+                }
+
                 return false;
             }
 

include/search/probfd/engines/topological_value_iteration.h
@@ -160,8 +160,18 @@ class TopologicalValueIteration : public MDPEngine<State, Action> {
         bool finalize()
         {
             if (!nconv_successors.empty()) {
-                assert(0_vt < self_loop_prob && self_loop_prob < 1_vt);
-                conv_part *= 1 / (1_vt - self_loop_prob);
+                if (self_loop_prob != 0_vt) {
+                    assert(0_vt < self_loop_prob && self_loop_prob < 1_vt);
+
+                    const auto normalization = 1 / (1_vt - self_loop_prob);
+
+                    for (auto& [_, prob] : nconv_successors) {
+                        prob *= normalization;
+                    }
+
+                    conv_part *= normalization;
+                }
+
                 return false;
             }
 