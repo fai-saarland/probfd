12e6a1288924db87f008765fd70c605dc9667237
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-05-16 11:36:08 +0200

    move tests into common file

--- tests/engine_tests.cpp
+++ tests/engine_tests.cpp
@@ -26,7 +26,88 @@ using namespace probfd::engine_interfaces;
 using namespace probfd::engines;
 using namespace probfd::tests;
 
-TEST(EngineTests, test_ilao_blocksworld_6b_initial_value)
+TEST(EngineTests, test_interval_set_min)
+{
+    Interval interval(8.0_vt, 40.0_vt);
+    Interval interval2(-45.0_vt, 30.0_vt);
+
+    probfd::engines::set_min(interval, interval2);
+
+    ASSERT_EQ(interval.lower, -45.0_vt);
+    ASSERT_EQ(interval.upper, 30.0_vt);
+}
+
+TEST(EngineTests, test_interval_set_min2)
+{
+    Interval interval(8.0_vt, INFINITE_VALUE);
+    Interval interval2(INFINITE_VALUE, 30.0_vt);
+
+    probfd::engines::set_min(interval, interval2);
+
+    ASSERT_EQ(interval.lower, 8.0_vt);
+    ASSERT_EQ(interval.upper, 30.0_vt);
+}
+
+TEST(EngineTests, test_interval_set_min3)
+{
+    Interval interval(INFINITE_VALUE, INFINITE_VALUE);
+    Interval interval2(-INFINITE_VALUE, INFINITE_VALUE);
+
+    probfd::engines::set_min(interval, interval2);
+
+    ASSERT_EQ(interval.lower, -INFINITE_VALUE);
+    ASSERT_EQ(interval.upper, INFINITE_VALUE);
+}
+
+TEST(EngineTests, test_interval_update1)
+{
+    Interval interval(8.0_vt, 40.0_vt);
+    Interval interval2(-45.0_vt, 30.0_vt);
+
+    bool result = probfd::engines::update(interval, interval2);
+
+    ASSERT_TRUE(result);
+    ASSERT_EQ(interval.lower, std::max(8.0_vt, -45.0_vt));
+    ASSERT_EQ(interval.upper, std::min(40.0_vt, 30.0_vt));
+}
+
+TEST(EngineTests, test_interval_update2)
+{
+    Interval interval(8.0_vt, 40.0_vt);
+    Interval interval2(7.0_vt, 41.0_vt);
+
+    bool result = probfd::engines::update(interval, interval2);
+
+    ASSERT_FALSE(result);
+    ASSERT_EQ(interval.lower, std::max(8.0_vt, 7.0_vt));
+    ASSERT_EQ(interval.upper, std::min(40.0_vt, 41.0_vt));
+}
+
+TEST(EngineTests, test_interval_update3)
+{
+    Interval interval(8.0_vt, 40.0_vt);
+    Interval interval2(25.0_vt, 30.0_vt);
+
+    bool result = probfd::engines::update(interval, interval2, false);
+
+    ASSERT_TRUE(result);
+    ASSERT_EQ(interval.lower, std::max(8.0_vt, 25.0_vt));
+    ASSERT_EQ(interval.upper, std::min(40.0_vt, 30.0_vt));
+}
+
+TEST(EngineTests, test_interval_update4)
+{
+    Interval interval(8.0_vt, 40.0_vt);
+    Interval interval2(7.0_vt, 39.0_vt);
+
+    bool result = probfd::engines::update(interval, interval2, false);
+
+    ASSERT_FALSE(result);
+    ASSERT_EQ(interval.lower, std::max(8.0_vt, 7.0_vt));
+    ASSERT_EQ(interval.upper, std::min(40.0_vt, 39.0_vt));
+}
+
+TEST(EngineTests, test_ilao_blocksworld_6_blocks)
 {
     std::shared_ptr<ProbabilisticTask> task(new BlocksworldTask(
         6,
@@ -77,7 +158,7 @@ TEST(EngineTests, test_ilao_blocksworld_6b_initial_value)
         state_space.get_state_id(state_space.get_initial_state())));
 }
 
-TEST(EngineTests, test_fret_ilao_blocksworld_6b_initial_value)
+TEST(EngineTests, test_fret_ilao_blocksworld_6_blocks)
 {
     std::shared_ptr<ProbabilisticTask> task(new BlocksworldTask(
         6,
