1433c9301e658882c8dde067781ba21eef5649c3
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-08-27 17:53:45 +0200

    move policy extraction to PDB utils

--- include/search/probfd/heuristics/pdbs/utils.h
+++ include/search/probfd/heuristics/pdbs/utils.h
@@ -9,6 +9,7 @@
 
 #include "probfd/value_type.h"
 
+#include <memory>
 #include <vector>
 
 namespace pdbs {
@@ -52,6 +53,38 @@ void compute_saturated_costs(
     ProjectionStateSpace& state_space,
     const std::vector<value_t>& value_table,
     std::vector<value_t>& saturated_costs);
+
+/**
+ * @brief Extract an abstract optimal policy for this projection from the given
+ * value table.
+ *
+ * Tie-breaking is performed randomly using the input RNG. If the wildcard
+ * option is specified, a wildcard policy will be returned, i.e., a policy that
+ * assigns multiple equivalent operators to a abstract state.
+ */
+[[nodiscard]] std::unique_ptr<ProjectionMultiPolicy>
+compute_optimal_projection_policy(
+    ProjectionStateSpace& mdp,
+    StateRank initial_state,
+    const std::vector<value_t>& value_table,
+    utils::RandomNumberGenerator& rng,
+    bool wildcard);
+
+/**
+ * @brief Extract a greedy policy for this projection from the given value
+ * table, which may not be optimal if traps are existent.
+ *
+ * Tie-breaking is performed randomly using the input RNG. If the wildcard
+ * option is specified, a wildcard policy will be returned, i.e., a policy that
+ * assigns multiple equivalent operators to a abstract state.
+ */
+[[nodiscard]] std::unique_ptr<ProjectionMultiPolicy>
+compute_greedy_projection_policy(
+    ProjectionStateSpace& state_space,
+    StateRank initial_state,
+    const std::vector<value_t>& value_table,
+    utils::RandomNumberGenerator& rng,
+    bool wildcard);
 
 } // namespace pdbs
 } // namespace heuristics
