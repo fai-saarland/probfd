cd84a96b980eb22e3f34c38eabc217201bf42981
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2023-11-24 07:51:24 +0100

    added bisimulation shrinking strategy

--- cmake/ProbFDFiles.cmake
+++ cmake/ProbFDFiles.cmake
@@ -372,6 +372,7 @@ create_library(
         probfd/merge_and_shrink/shrink_strategy_equal_distance
         probfd/merge_and_shrink/shrink_strategy_identity
         probfd/merge_and_shrink/shrink_strategy_random
+        probfd/merge_and_shrink/shrink_strategy_bisimulation
         probfd/merge_and_shrink/prune_strategy_identity
         probfd/merge_and_shrink/transition_system
         probfd/merge_and_shrink/types
