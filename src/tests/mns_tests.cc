f6515c86ace93ee7513294f45850ecec47571a86
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2024-02-15 16:48:21 +0100

    added two more atomic transition system tests

--- src/tests/mns_tests.cc
+++ src/tests/mns_tests.cc
@@ -8,9 +8,14 @@
 
 #include "probfd/task_proxy.h"
 
+#include "probfd/utils/json.h"
+
 #include "downward/utils/logging.h"
 
 #include "tests/tasks/blocksworld.h"
+
+#include <format>
+#include <fstream>
 
 using namespace probfd;
 using namespace probfd::merge_and_shrink;
@@ -26,85 +31,65 @@ TEST(MnSTests, test_atomic_fts)
     FactoredTransitionSystem fts =
         create_factored_transition_system(task_proxy, false, false, log);
 
-    ASSERT_EQ(fts.get_size(), 3) << "Unexpected number of atomic factors!";
+    ASSERT_EQ(fts.get_size(), task.get_num_variables())
+        << "Unexpected number of atomic factors!";
 
-    auto& ts0 = fts.get_transition_system(0);
-    auto& ts1 = fts.get_transition_system(1);
-    auto& ts2 = fts.get_transition_system(2);
+    for (int i = 0; i != fts.get_size(); ++i) {
+        const auto& ts = fts.get_transition_system(i);
+        std::ifstream file(
+            std::format("resources/mns_tests/atomic_ts_bw1_{}.json", i));
+        const auto expected_ts = json::read<TransitionSystem>(file);
+        ASSERT_EQ(ts, expected_ts) << std::format(
+            "Transition system {} did not match the expected transition "
+            "system!",
+            i);
+    }
+}
 
-    ASSERT_EQ(ts0.get_size(), 2) << "Unexpected size of transitions system 0!";
-    ASSERT_EQ(ts1.get_size(), 3) << "Unexpected size of transitions system 1!";
-    ASSERT_EQ(ts2.get_size(), 2) << "Unexpected size of transitions system 2!";
+TEST(MnSTests, test_atomic_fts2)
+{
+    BlocksworldTask task(2, {{0, 1}}, {{0, 1}});
 
-    ASSERT_EQ(ts0.get_incorporated_variables(), std::vector{0});
-    ASSERT_EQ(ts1.get_incorporated_variables(), std::vector{1});
-    ASSERT_EQ(ts2.get_incorporated_variables(), std::vector{2});
+    ProbabilisticTaskProxy task_proxy(task);
+    utils::LogProxy log(std::make_shared<utils::Log>(utils::Verbosity::DEBUG));
+    FactoredTransitionSystem fts =
+        create_factored_transition_system(task_proxy, false, false, log);
 
-    ASSERT_EQ(ts0.get_init_state(), 1);
-    ASSERT_EQ(ts1.get_init_state(), 1);
-    ASSERT_EQ(ts2.get_init_state(), 1);
+    ASSERT_EQ(fts.get_size(), task.get_num_variables())
+        << "Unexpected number of atomic factors!";
 
-    ASSERT_TRUE(ts0.is_goal_state(0));
-    ASSERT_TRUE(ts0.is_goal_state(1));
-    ASSERT_TRUE(!ts1.is_goal_state(0));
-    ASSERT_TRUE(ts1.is_goal_state(1));
-    ASSERT_TRUE(!ts1.is_goal_state(2));
-    ASSERT_TRUE(ts2.is_goal_state(0));
-    ASSERT_TRUE(ts2.is_goal_state(1));
+    for (int i = 0; i != fts.get_size(); ++i) {
+        const auto& ts = fts.get_transition_system(i);
+        std::ifstream file(
+            std::format("resources/mns_tests/atomic_ts_bw2_{}.json", i));
+        const auto expected_ts = json::read<TransitionSystem>(file);
+        ASSERT_EQ(ts, expected_ts) << std::format(
+            "Transition system {} did not match the expected transition "
+            "system!",
+            i);
+    }
+}
 
-    std::ranges::input_range auto label_infos0 = ts0.label_infos().base();
-    std::ranges::input_range auto label_infos1 = ts1.label_infos().base();
-    std::ranges::input_range auto label_infos2 = ts2.label_infos().base();
+TEST(MnSTests, test_atomic_fts3)
+{
+    BlocksworldTask task(3, {{0, 1, 2}}, {{0, 1, 2}});
 
-    ASSERT_EQ(label_infos0.size(), 2);
+    ProbabilisticTaskProxy task_proxy(task);
+    utils::LogProxy log(std::make_shared<utils::Log>(utils::Verbosity::DEBUG));
+    FactoredTransitionSystem fts =
+        create_factored_transition_system(task_proxy, false, false, log);
 
-    ASSERT_EQ(label_infos0[0].get_cost(), 1);
-    ASSERT_EQ(
-        label_infos0[0].get_probabilities(),
-        (std::vector{0.75_vt, 0.25_vt}));
-    ASSERT_EQ(label_infos0[0].get_label_group(), std::vector{0});
-    ASSERT_EQ(label_infos0[0].get_transitions().size(), 1);
-    ASSERT_EQ(label_infos0[0].get_transitions()[0].src, 1);
-    ASSERT_EQ(
-        label_infos0[0].get_transitions()[0].targets,
-        (std::vector{1, 1}));
+    ASSERT_EQ(fts.get_size(), task.get_num_variables())
+        << "Unexpected number of atomic factors!";
 
-    ASSERT_EQ(label_infos0[1].get_cost(), 0);
-    ASSERT_EQ(label_infos0[1].get_probabilities(), (std::vector{1_vt}));
-    ASSERT_EQ(label_infos0[1].get_label_group(), std::vector{1});
-    ASSERT_EQ(label_infos0[1].get_transitions().size(), 1);
-    ASSERT_EQ(label_infos0[1].get_transitions()[0], Transition(1, {1}));
-
-    ASSERT_EQ(label_infos1.size(), 2);
-
-    ASSERT_EQ(label_infos1[0].get_cost(), 1);
-    ASSERT_EQ(
-        label_infos1[0].get_probabilities(),
-        (std::vector{0.75_vt, 0.25_vt}));
-    ASSERT_EQ(label_infos1[0].get_label_group(), std::vector{0});
-    ASSERT_EQ(label_infos1[0].get_transitions().size(), 1);
-    ASSERT_EQ(label_infos1[0].get_transitions()[0], Transition(1, {2, 1}));
-
-    ASSERT_EQ(label_infos1[1].get_cost(), 0);
-    ASSERT_EQ(label_infos1[1].get_probabilities(), (std::vector{1_vt}));
-    ASSERT_EQ(label_infos1[1].get_label_group(), std::vector{1});
-    ASSERT_EQ(label_infos1[1].get_transitions().size(), 1);
-    ASSERT_EQ(label_infos1[1].get_transitions()[0], Transition(2, {1}));
-
-    ASSERT_EQ(label_infos2.size(), 2);
-
-    ASSERT_EQ(label_infos2[0].get_cost(), 1);
-    ASSERT_EQ(
-        label_infos2[0].get_probabilities(),
-        (std::vector{0.75_vt, 0.25_vt}));
-    ASSERT_EQ(label_infos2[0].get_label_group(), std::vector{0});
-    ASSERT_EQ(label_infos2[0].get_transitions().size(), 1);
-    ASSERT_EQ(label_infos2[0].get_transitions()[0], Transition(1, {0, 1}));
-
-    ASSERT_EQ(label_infos2[1].get_cost(), 0);
-    ASSERT_EQ(label_infos2[1].get_probabilities(), (std::vector{1_vt}));
-    ASSERT_EQ(label_infos2[1].get_label_group(), std::vector{1});
-    ASSERT_EQ(label_infos2[1].get_transitions().size(), 2);
-    ASSERT_EQ(label_infos2[1].get_transitions()[0], Transition(0, {1}));
-    ASSERT_EQ(label_infos2[1].get_transitions()[1], Transition(1, {1}));
+    for (int i = 0; i != fts.get_size(); ++i) {
+        const auto& ts = fts.get_transition_system(i);
+        std::ifstream file(
+            std::format("resources/mns_tests/atomic_ts_bw3_{}.json", i));
+        const auto expected_ts = json::read<TransitionSystem>(file);
+        ASSERT_EQ(ts, expected_ts) << std::format(
+            "Transition system {} did not match the expected transition "
+            "system!",
+            i);
+    }
 }
