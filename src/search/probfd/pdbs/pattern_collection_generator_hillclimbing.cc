2142df0a2be37ace8ff49e2269dc20f31f0fbc05
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2024-03-27 19:23:58 +0100

    fix default generator for hillclimbing generator

--- src/search/probfd/pdbs/pattern_collection_generator_hillclimbing.cc
+++ src/search/probfd/pdbs/pattern_collection_generator_hillclimbing.cc
@@ -819,7 +819,7 @@ static void add_hillclimbing_options(plugins::Feature& feature)
     feature.add_option<std::shared_ptr<PatternCollectionGenerator>>(
         "initial_generator",
         "generator for the initial pattern database ",
-        "det_adapter(generator=systematic(pattern_max_size=1))");
+        "classical_generator(generator=systematic(pattern_max_size=1))");
 
     feature.add_option<std::shared_ptr<SubCollectionFinderFactory>>(
         "subcollection_finder_factory",
