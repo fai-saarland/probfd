cffd84e8164d7708e2ba6f31a433a5640715f14b
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2024-04-08 12:36:08 +0200

    added probabilistic bisimulation shrink strategy

---
+++ include/probfd/merge_and_shrink/shrink_strategy_probabilistic_bisimulation.h
@@ -0,0 +1,44 @@
+#ifndef PROBFD_MERGE_AND_SHRINK_SHRINK_STRATEGY_PROBABILISTIC_BISIMULATION_H
+#define PROBFD_MERGE_AND_SHRINK_SHRINK_STRATEGY_PROBABILISTIC_BISIMULATION_H
+
+#include "probfd/merge_and_shrink/shrink_strategy.h"
+
+namespace plugins {
+class Options;
+}
+
+namespace probfd::merge_and_shrink {
+
+enum class AtLimit { RETURN, USE_UP };
+
+class ShrinkStrategyProbabilisticBisimulation : public ShrinkStrategy {
+    const AtLimit at_limit;
+
+public:
+    explicit ShrinkStrategyProbabilisticBisimulation(AtLimit at_limit);
+
+    StateEquivalenceRelation compute_equivalence_relation(
+        const TransitionSystem& ts,
+        const Distances& distances,
+        int target_size,
+        utils::LogProxy& log) const override;
+
+    bool requires_liveness() const override { return false; }
+    bool requires_goal_distances() const override { return true; }
+
+protected:
+    void dump_strategy_specific_options(utils::LogProxy& log) const override;
+
+    std::string name() const override;
+
+private:
+    void compute_abstraction(
+        const TransitionSystem& ts,
+        const Distances& distances,
+        int target_size,
+        StateEquivalenceRelation& equivalence_relation) const;
+};
+
+} // namespace probfd::merge_and_shrink
+
+#endif
