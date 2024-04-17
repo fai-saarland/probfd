a017872be98a881372e1544c403b8568bfcb090a
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2024-04-17 17:31:59 +0200

    added cg scc merge strategy

--- cmake/ProbFDFiles.cmake
+++ cmake/ProbFDFiles.cmake
@@ -355,10 +355,10 @@ create_library(
         probfd/merge_and_shrink/merge_strategy
         probfd/merge_and_shrink/merge_strategy_factory
         probfd/merge_and_shrink/merge_strategy_factory_precomputed
-        #probfd/merge_and_shrink/merge_strategy_factory_sccs
+        probfd/merge_and_shrink/merge_strategy_factory_sccs
         probfd/merge_and_shrink/merge_strategy_factory_stateless
         probfd/merge_and_shrink/merge_strategy_precomputed
-        #probfd/merge_and_shrink/merge_strategy_sccs
+        probfd/merge_and_shrink/merge_strategy_sccs
         probfd/merge_and_shrink/merge_strategy_stateless
         probfd/merge_and_shrink/merge_tree
         probfd/merge_and_shrink/merge_tree_factory
---
+++ include/probfd/merge_and_shrink/merge_strategy_factory_sccs.h
@@ -0,0 +1,51 @@
+#ifndef PROBFD_MERGE_AND_SHRINK_MERGE_STRATEGY_FACTORY_SCCS_H
+#define PROBFD_MERGE_AND_SHRINK_MERGE_STRATEGY_FACTORY_SCCS_H
+
+#include "probfd/merge_and_shrink/merge_strategy_factory.h"
+
+namespace downward::cli::plugins {
+class Feature;
+class Options;
+} // namespace downward::cli::plugins
+
+namespace probfd::merge_and_shrink {
+class MergeTreeFactory;
+class MergeSelector;
+} // namespace probfd::merge_and_shrink
+
+namespace probfd::merge_and_shrink {
+
+enum class OrderOfSCCs {
+    TOPOLOGICAL,
+    REVERSE_TOPOLOGICAL,
+    DECREASING,
+    INCREASING
+};
+
+class MergeStrategyFactorySCCs : public MergeStrategyFactory {
+    OrderOfSCCs order_of_sccs;
+    std::shared_ptr<MergeTreeFactory> merge_tree_factory;
+    std::shared_ptr<MergeSelector> merge_selector;
+
+public:
+    MergeStrategyFactorySCCs(
+        utils::Verbosity verbosity,
+        OrderOfSCCs order_of_sccs,
+        std::shared_ptr<MergeTreeFactory> merge_tree_factory,
+        std::shared_ptr<MergeSelector> merge_selector);
+
+    std::unique_ptr<MergeStrategy> compute_merge_strategy(
+        std::shared_ptr<ProbabilisticTask>& task,
+        const FactoredTransitionSystem& fts) override;
+
+    bool requires_liveness() const override;
+    bool requires_goal_distances() const override;
+
+protected:
+    std::string name() const override;
+    void dump_strategy_specific_options() const override;
+};
+
+} // namespace probfd::merge_and_shrink
+
+#endif
