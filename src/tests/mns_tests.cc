51593ea9d2e8b89c247bccef593d14e9df04c3bd
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2024-04-17 10:15:08 +0200

    fix broken test compilation

--- src/tests/mns_tests.cc
+++ src/tests/mns_tests.cc
@@ -291,7 +291,7 @@ TEST(MnSTests, test_bisimulation_distance_preserved)
     Distances distances;
     distances.compute_distances(ts, false, log);
 
-    ShrinkBisimulation bisimulation(AtLimit::RETURN);
+    ShrinkBisimulation bisimulation(AtLimit::RETURN, true);
 
     auto eq_relation = bisimulation.compute_equivalence_relation(
         ts,
