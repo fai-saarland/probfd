b83d7c0e95ee5fa60c61171b5af9def81011fd95
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2025-01-14 15:13:32 +0100

    make MultiFeaturePlugin generic

--- include/probfd/cli/multi_feature_plugin.h
+++ include/probfd/cli/multi_feature_plugin.h
@@ -1,45 +1,69 @@
-#ifndef PROBFD_PLUGINS_MULTI_FEATURE_PLUGIN_H
-#define PROBFD_PLUGINS_MULTI_FEATURE_PLUGIN_H
+#ifndef CLI_MULTI_FEATURE_PLUGIN_H
+#define CLI_MULTI_FEATURE_PLUGIN_H
 
 #include "downward/cli/plugins/plugin.h"
 
 namespace probfd::cli {
 
-template <template <bool, bool> typename T>
-class MultiCategoryPlugin {
-    static_assert(std::is_base_of_v<
-                  downward::cli::plugins::CategoryPlugin,
-                  T<false, false>>);
-    static_assert(std::is_base_of_v<
-                  downward::cli::plugins::CategoryPlugin,
-                  T<false, true>>);
-    static_assert(std::is_base_of_v<
-                  downward::cli::plugins::CategoryPlugin,
-                  T<true, false>>);
-    static_assert(std::is_base_of_v<
-                  downward::cli::plugins::CategoryPlugin,
-                  T<true, true>>);
+template <typename T>
+static constexpr bool is_category_plugin =
+    std::is_base_of_v<downward::cli::plugins::CategoryPlugin, T>;
 
-    T<false, false> category_plugin_1_;
-    T<false, true> category_plugin_2_;
-    T<true, false> category_plugin_3_;
-    T<true, true> category_plugin_4_;
+template <template <bool...> typename T, bool... b>
+concept instantiable = requires { typename T<b...>; };
+
+template <template <bool...> typename T, bool... b>
+struct suffix_instantiable_h : std::false_type {};
+
+template <template <bool...> typename T, bool b, bool... tail>
+struct suffix_instantiable_h<T, b, tail...>
+    : std::bool_constant<
+          instantiable<T, b, tail...> ||
+          suffix_instantiable_h<T, tail...>::value> {};
+
+template <template <bool...> typename T, bool... b>
+concept suffix_instantiable = suffix_instantiable_h<T, b...>::value;
+
+template <template <bool...> typename T, bool... b>
+concept strict_suffix_instantiable =
+    suffix_instantiable<T, b...> && !instantiable<T, b...>;
+
+template <template <bool...> typename T, bool... b>
+concept partial_specialization = !strict_suffix_instantiable<T, b...>;
+
+template <template <bool...> typename T, bool... b>
+    requires partial_specialization<T, b...>
+struct MultiCategoryPlugin;
+
+template <template <bool...> typename T, bool... b>
+    requires partial_specialization<T, b...> && instantiable<T, b...> &&
+             is_category_plugin<T<b...>>
+struct MultiCategoryPlugin<T, b...> {
+    T<b...> plugin_;
 };
 
-template <template <bool> typename T>
-class BinaryFeaturePlugin {
-    downward::cli::plugins::FeaturePlugin<T<false>> plugin_1_;
-    downward::cli::plugins::FeaturePlugin<T<true>> plugin_2_;
+template <template <bool...> typename T, bool... b>
+    requires partial_specialization<T, b...> && (!instantiable<T, b...>)
+struct MultiCategoryPlugin<T, b...>
+    : MultiCategoryPlugin<T, b..., true>
+    , MultiCategoryPlugin<T, b..., false> {};
+
+template <template <bool...> typename T, bool... b>
+    requires partial_specialization<T, b...>
+struct MultiFeaturePlugin;
+
+template <template <bool...> typename T, bool... b>
+    requires partial_specialization<T, b...> && instantiable<T, b...>
+struct MultiFeaturePlugin<T, b...> {
+    downward::cli::plugins::FeaturePlugin<T<b...>> plugin_;
 };
 
-template <template <bool, bool> typename T>
-class MultiFeaturePlugin {
-    downward::cli::plugins::FeaturePlugin<T<false, false>> plugin_1_;
-    downward::cli::plugins::FeaturePlugin<T<false, true>> plugin_2_;
-    downward::cli::plugins::FeaturePlugin<T<true, false>> plugin_3_;
-    downward::cli::plugins::FeaturePlugin<T<true, true>> plugin_4_;
-};
+template <template <bool...> typename T, bool... b>
+    requires partial_specialization<T, b...> && (!instantiable<T, b...>)
+struct MultiFeaturePlugin<T, b...>
+    : MultiFeaturePlugin<T, b..., true>
+    , MultiFeaturePlugin<T, b..., false> {};
 
 } // namespace probfd::cli
 
-#endif
+#endif // CLI_MULTI_FEATURE_PLUGIN_H
