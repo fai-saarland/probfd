33b04c733792dc8d6447a9fa0b94a50a4326eb91
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2025-07-20 22:59:42 +0200

    fix bad parameter order in cli constructions

--- src/probfd/cli/cartesian_abstractions/subtask_generators.cc
+++ src/probfd/cli/cartesian_abstractions/subtask_generators.cc
@@ -79,6 +79,7 @@ public:
             "combine_facts",
             "combine landmark facts with domain abstraction",
             "true");
+        add_rng_options_to_feature(*this);
     }
 
     std::shared_ptr<LandmarkDecomposition>
