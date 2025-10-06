e6de05c20cc86f529a9e53f86af3720e72af02ae
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2025-10-06 11:34:24 +0200

    [bugfix] fix return value of SingleCEGAR::get_flaws for unsolvable case

--- src/probfd/pdbs/cegar/single_cegar.cc
+++ src/probfd/pdbs/cegar/single_cegar.cc
@@ -118,7 +118,7 @@ bool SingleCEGAR::get_flaws(
         policy->get_decisions(init_state_rank).empty()) {
         log.println("SingleCEGAR: Problem unsolvable");
         log.println("SingleCEGAR: Unsolvable pattern: {}", pdb.get_pattern());
-        return true;
+        return false;
     }
 
     const auto& variables = get_variables(task);
