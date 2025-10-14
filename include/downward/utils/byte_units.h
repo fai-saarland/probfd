31d199f9eb859497f3e4489687886bd65f97d82d
Thorsten Klößner <kloessner@cs.uni-saarland.de>
2025-10-14 12:13:07 +0200

    further adapt unit headers

--- include/downward/utils/byte_units.h
+++ include/downward/utils/byte_units.h
@@ -10,68 +10,141 @@
 
 namespace downward::utils {
 
+/// Represents a size in bytes
 template <ArithmeticType Rep, RatioType Period>
 struct ByteSize : public unit<Rep, Period> {
     // Inherit constructor
     using ByteSize::unit::unit;
 };
 
+/// Size in bytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GBytes = ByteSize<T, std::ratio<1>>;
+
+/// Size in kilobytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GKilobytes = ByteSize<T, std::kilo>;
+
+/// Size in megabytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GMegabytes = ByteSize<T, std::mega>;
+
+/// Size in gigabytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GGigabytes = ByteSize<T, std::giga>;
+
+/// Size in terabytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GTerabytes = ByteSize<T, std::tera>;
+
+/// Size in petabytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GPetabytes = ByteSize<T, std::peta>;
+
+/// Size in exabytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GExabytes = ByteSize<T, std::exa>;
 
+/// Size in kibibytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GKibibytes = ByteSize<T, kibi>;
+
+/// Size in mebibytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GMebibytes = ByteSize<T, mebi>;
+
+/// Size in gibibytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GGibibytes = ByteSize<T, gibi>;
+
+/// Size in tebibytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GTebibytes = ByteSize<T, tebi>;
+
+/// Size in pebibytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GPebibytes = ByteSize<T, pebi>;
+
+/// Size in exbibytes represented using arithmetic type T.
 template <ArithmeticType T>
 using GExbibytes = ByteSize<T, exbi>;
 
+/// Size in bytes represented using std::intmax_t.
 using Bytes = GBytes<std::intmax_t>;
+
+/// Size in kilobytes represented using std::intmax_t.
 using Kilobytes = GKilobytes<std::intmax_t>;
+
+/// Size in megabytes represented using std::intmax_t.
 using Megabytes = GMegabytes<std::intmax_t>;
+
+/// Size in gigabytes represented using std::intmax_t.
 using Gigabytes = GGigabytes<std::intmax_t>;
+
+/// Size in terabytes represented using std::intmax_t.
 using Terabytes = GTerabytes<std::intmax_t>;
+
+/// Size in petabytes represented using std::intmax_t.
 using Petabytes = GPetabytes<std::intmax_t>;
+
+/// Size in exabytes represented using std::intmax_t.
 using Exabytes = GExabytes<std::intmax_t>;
 
+/// Size in kibibytes represented using std::intmax_t.
 using Kibibytes = GKibibytes<std::intmax_t>;
+
+/// Size in mebibytes represented using std::intmax_t.
 using Mebibytes = GMebibytes<std::intmax_t>;
+
+/// Size in gibibytes represented using std::intmax_t.
 using Gibibytes = GGibibytes<std::intmax_t>;
+
+/// Size in tebibytes represented using std::intmax_t.
 using Tebibytes = GTebibytes<std::intmax_t>;
+
+/// Size in pebibytes represented using std::intmax_t.
 using Pebibytes = GPebibytes<std::intmax_t>;
+
+/// Size in exbibytes represented using std::intmax_t.
 using Exbibytes = GExbibytes<std::intmax_t>;
 
+/// Size in bytes represented using long double.
 using FBytes = GBytes<long double>;
+
+/// Size in kilobytes represented using long double.
 using FKilobytes = GKilobytes<long double>;
+
+/// Size in megabytes represented using long double.
 using FMegabytes = GMegabytes<long double>;
+
+/// Size in gigabytes represented using long double.
 using FGigabytes = GGigabytes<long double>;
+
+/// Size in terabytes represented using long double.
 using FTerabytes = GTerabytes<long double>;
+
+/// Size in petabytes represented using long double.
 using FPetabytes = GPetabytes<long double>;
+
+/// Size in exabytes represented using long double.
 using FExabytes = GExabytes<long double>;
 
+/// Size in kibibytes represented using long double.
 using FKibibytes = GKibibytes<long double>;
+
+/// Size in mebibytes represented using long double.
 using FMebibytes = GMebibytes<long double>;
+
+/// Size in gibibytes represented using long double.
 using FGibibytes = GGibibytes<long double>;
+
+/// Size in tebibytes represented using long double.
 using FTebibytes = GTebibytes<long double>;
+
+/// Size in pebibytes represented using long double.
 using FPebibytes = GPebibytes<long double>;
+
+/// Size in exbibytes represented using long double.
 using FExbibytes = GExbibytes<long double>;
 
 template <ArithmeticType T>
